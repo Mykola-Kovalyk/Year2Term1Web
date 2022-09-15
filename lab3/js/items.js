6b3d7c4045ace426ed8724448c89c729e66d31d9
Nicolas <93177243+MykolaKovalyk@users.noreply.github.com>
2022-09-15 11:09:17 +0300

    Add additional code for lab3

--- lab3/js/items.js
+++ lab3/js/items.js
@@ -1,36 +1,29 @@
-const EDIT_BUTTON_PREFIX = "edit-";
-const DELETE_BUTTON_PREFIX = "delete-";
+import { showModal } from "./modal.js";
+import { itemTemplate, EDIT_BUTTON_PREFIX, DELETE_BUTTON_PREFIX } from "./template.js"; 
+
+
+
 
 const dataList = document.getElementById("hero_data-list");
 const titleField = document.getElementById("hero_data-field-title");
 const descriptionField = document.getElementById("hero_data-field-description");
+const slotsField =  document.getElementById("hero_data-field-parking-slots");
 const submitButton = document.getElementById("hero_data-field-add-item");
+const sortButton = document.getElementById("hero_data-field-sort");
+const filterInput = document.getElementById("hero_data-field-search");
+const filterButton = document.getElementById("hero_data-field-filter");
+const totalSlotsField = document.getElementById("hero_data-field-slots");
 
-let parkingFacilities = {};
+let parkingFacilities = [];
 let idCounter = 0;
 let editMode = false;
-let itemToEdit = null;
+let itemToEditIndex = -1;
 
-const itemTemplate = ({id, title, description}) => `
-                <li id="${id}" class="hero_data-list-item">
-                    <img class="hero_data-list-item-img" src="media/parking.png" alt="parking.png">
-                    
-                    &emsp;<h2 class="hero_data-list-item-title">${title}</h2>
-                    
-                    <div class="hero_data-list-item-text" style="font-size: small; padding: 20px;">
-                    ${description}
-                    </div>
-                    
-                    <div class="hero_data-list-item-buttons" style="display: flex;">
-                        <button class="hero_data-list-item-edit" id="edit-${id}" onclick="editItem(this)">Edit</button>
-                        <button class="hero_data-list-item-delete" id="delete-${id}" onclick="deleteItem(this)">Delete</button>
-                    </div>
-                </li>
-`;
 
-function addItem() {
 
-    if(!titleField.checkValidity() || !descriptionField.checkValidity())
+window.addItem = function() {
+
+    if(!titleField.checkValidity() || !descriptionField.checkValidity() || !slotsField.checkValidity())
     {
         showModal("Oops!", "<p>The text you entered is in the wrong format. Check if you entered a valid title and text.</p>", []);
         return;
@@ -39,21 +32,23 @@ function addItem() {
 
     if(editMode)
     {
-        parkingFacilities[itemToEdit.id] = { id: itemToEdit.id, title: titleField.value, description: descriptionField.value };
+        var oldElement = parkingFacilities[itemToEditIndex]
+        parkingFacilities[itemToEditIndex] = { id: oldElement.id, title: titleField.value, description: descriptionField.value, slots: parseInt(slotsField.value) };
         editMode = false;
     }
     else
     {
-        parkingFacilities[idCounter] = { id: idCounter, title: titleField.value, description: descriptionField.value };
+        parkingFacilities.push({ id: idCounter, title: titleField.value, description: descriptionField.value, slots: parseInt(slotsField.value) });
         idCounter++;
     }
 
     titleField.value = "";
     descriptionField.value = "";
-    refreshList();
+    slotsField.value = ""
+    refreshList(parkingFacilities);
 }
 
-function editItem(editButton) {
+window.editItem = function(editButton) {
     const htmlItem = document.getElementById(editButton.id.replace(EDIT_BUTTON_PREFIX, ""));
     
 
@@ -62,61 +57,40 @@ function editItem(editButton) {
     titleField.value = title;
     descriptionField.value = text;
 
-    itemToEdit = parkingFacilities[htmlItem.id];
+    itemToEditIndex = parkingFacilities.findIndex(element => element.id = htmlItem.id);
     editMode = true;
 }
 
 
-function deleteItem (deleteButton) {
+window.deleteItem = function(deleteButton) {
     const item = document.getElementById(deleteButton.id.replace(DELETE_BUTTON_PREFIX, ""));
-    delete parkingFacilities[item.id];
-    refreshList();
+    parkingFacilities = parkingFacilities.filter(element => element.id !== parseInt(item.id))
+    refreshList(parkingFacilities);
 }
 
-function refreshList() {
+
+
+window.refreshList = function(itemList) {
     dataList.innerHTML = "";
-    for (const [id, facility] of Object.entries(parkingFacilities)) {
+
+    var totalSlots = 0
+    for (const facility of itemList) {
         dataList.insertAdjacentHTML("afterbegin", itemTemplate(facility));
-    }
-}
-
-function showModal(titleHtml, contentHtml, buttons) {
-    const modal = document.createElement("div");
-
-    modal.classList.add("modal");
-    modal.innerHTML = `
-            <div class="modal__inner">
-                <div class="modal__top">
-                    <div class="modal__title">${titleHtml}</div>
-                    <button class="modal__close" type="button">
-                        <span class="material-icons">close</span>
-                    </button>
-                </div>
-                <div class="modal__content">${contentHtml}</div>
-                <div class="modal__bottom"></div>
-            </div>
-        `;
-
-    for (const button of buttons) {
-        const element = document.createElement("button");
-
-        element.setAttribute("type", "button");
-        element.classList.add("modal__button");
-        element.textContent = button.label;
-        element.addEventListener("click", () => {
-            if (button.triggerClose) {
-                document.body.removeChild(modal);
-            }
-
-            button.onClick(modal);
-        });
-
-        modal.querySelector(".modal__bottom").appendChild(element);
+        totalSlots += facility.slots
     }
 
-    modal.querySelector(".modal__close").addEventListener("click", () => {
-        document.body.removeChild(modal);
-    });
-
-    document.body.appendChild(modal);
+    totalSlotsField.innerHTML = totalSlots
 }
+
+window.sortItems = function() {
+    parkingFacilities = parkingFacilities.sort((a, b) => a.slots - b.slots)
+    refreshList(parkingFacilities)
+}
+
+window.filterItems = function() {
+    var filterText = filterInput.value
+
+    var filteredArray = parkingFacilities.filter( (facility) => facility.title.search(filterText) !== -1 ||  facility.description.search(filterText) !== -1) 
+    refreshList(filteredArray)
+}
+
